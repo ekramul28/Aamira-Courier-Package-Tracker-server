a92b2dde52f596aa2b66f1ecf0f4ebfda8418245
Ekramul <mdekramulhassan168@gmail.com>
2025-07-21 21:52:35 -0700

    added all setup and admin user

---
+++ src/app/routes/index.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { AdminRoutes } from '../modules/Admin/admin.route';
+import { AuthRoutes } from '../modules/Auth/auth.route';
+
+import { StudentRoutes } from '../modules/Student/student.route';
+import { UserRoutes } from '../modules/User/user.route';
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
+  },
+
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
